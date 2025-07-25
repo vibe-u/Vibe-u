58041a445a0bd1f781bf9dca834cf45f14592969
Kyara2005 <kyara.altamirano@epn.edu.ec>
2025-07-25 04:06:21 -0500

    CRUD

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,8 @@
-
 import { NavLink } from "react-router"
 import { createUserWithEmailAndPassword } from "firebase/auth"
-import { authFirebase } from "../firebase.js"
+import { authFirebase, dbFirebase } from "../firebase";
+import {setDoc, doc} from 'firebase/firestore'
+import {setDoc, doc} from 'firebase/auth'
 import { useNavigate } from "react-router"
 import { useForm } from "react-hook-form";
 
@@ -18,59 +19,17 @@ const Register = () => {
             await createUserWithEmailAndPassword(authFirebase, email, password)
             const user = authFirebase.currentUser
             console.log(user)
+            if (user){
+                await setDoc(doc(dbFirebase,"Users",user.uid),{
+                    email:user.email,
+                    name:data.name,
+                    rol:"admin"
+                })
+            }
             navigate("/login")
         } catch (error) {
             console.log(error.message)
             alert(error.message)
         }
     }
-
-    return (
-        <>
-            <main className="contenido-principal contenedor ">
-                <h3 className="text-center">Registro</h3>
-                <form className="formulario" onSubmit={handleSubmit(handleRegister)}>
-
-                    <fieldset>
-
-                        <legend>Ingresa tus datos</legend>
-
-                        <div className="campo">
-                            <label >Nombre: </label>
-                            <input type="text" placeholder="Tu Nombre"
-                            {...register("name",{ required: true })}
-                            />
-                            {errors.name && <span className="errors">El nombre es requerido</span>}
-
-                        </div>
-
-                        <div className="campo">
-                            <label >E-mail:</label>
-                            <input type="email" placeholder="Tu Email"
-                            {...register("email",{ required: true })}
-                            />
-                            {errors.email && <span className="errors">El correo es requerido</span>}
-                        </div>
-
-                        <div className="campo">
-                            <label >Contraseña:</label>
-                            <input type="password" placeholder="Tu Contraseña"
-                            {...register("password",{ required: true })}
-                            />
-                            {errors.password && <span className="errors">La contraseña es requerida</span>}
-                        </div>
-
-                    </fieldset>
-
-                    <input className="btn" type="submit" value="Enviar" ></input>
-
-                </form>
-
-                <NavLink to="/login" className="enlace">Si ya tienes cuenta, puedes iniciar sesión aquí</NavLink>
-                
-            </main>
-        </>
-    )
 }
-
-export default Register
